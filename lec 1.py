3dbc8d483c3b130aaea9f6657faeb48261ee69d7
waqas <wchofficials128@gmail.com>
2023-01-19 17:35:28 +0500

    lec 1

---
+++ lec 1.py
@@ -0,0 +1,9 @@
+print('Hello world')
+print("Starting my new Python course ")
+print('Today task')
+print("Create a Github repositry uploade this file ")
+val = input("Enter yes if done: ")
+if val == 'yes':
+    print('Task completed for today ')
+else:
+    print('Not Completed yet complete the task')
